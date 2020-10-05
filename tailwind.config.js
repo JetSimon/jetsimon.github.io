93a76ec02b3604aa3cb0ad7d38d83952126921be
Jet Simon <jetjdsimon@gmail.com>
2020-10-05 12:41:03 -0700

    add site

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'background-brown': '#EFD6B4',
+        'rootin-red': '#EE7C7C',
+        'ogre-green': '#8EBC93',
+        'balls-blue':'#8EA9BA',
+        'texters-brown':'#7F6B5B',
+        'headers-brown':'#534741',
+
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
