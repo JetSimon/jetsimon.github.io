7a8f0433c1fb07d5f56d013b9608478e84655ddc
Jet Simon <jetjdsimon@gmail.com>
2021-05-02 21:39:57 -0700

    add v1 of portfolio
    uses json to store records so i dont have to edit html ever again. i will later on convert other pages to this so i dont shoot myself

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,16 +6,38 @@ Vue.component('portfolio-item', {
       },
     props: ['path'],
     template: `
-    <div class="bg-white rounded-lg shadow w-1/3 h-96 font-sans text-gray-600 px-5">
-        <h1 class="text-grey-800 text-center text-5xl mt-5">{{this.title}}</h1>
-        <p class="text-gray-400 text-sm italic text-center">{{techList}}</p>
+    <div class="bg-white text-center rounded-lg shadow w-5/6 font-sans text-gray-600 px-5">
+        <div class="grid grid-cols-1 md:grid-cols-2">
+        <div>
+            <h1 class="text-grey-800 text-center text-5xl mt-5">{{this.title}}</h1>
+            <p class="text-gray-400 text-xs px-10 italic text-center">{{techList}}</p>
+            <img class="mx-auto px-10 w-full py-5 object-cover" :src=this.image>
+        </div>
+        <div class="my-auto ">
+            <p class="text-gray-900 bg-gray-100 shadow px-8 py-3 rounded-lg mx-auto w-5/6 text-sm text-center">{{this.desc}}</p>
+            <a class="text-blue-500 text-xs hover:text-blue-700" :href=this.link>link to project</a>
+            <a class="text-blue-500 text-xs hover:text-blue-700" :href=this.source>source code</a>
+        
+            </div>
+        </div>
     </div>
     `,
     computed: {
         techList:function(){
           return "made with " + this.tech.join(", ")
         }
-      }
+    },
+    mounted() {
+        fetch('/json/' + this.path).then(response => response.json())
+        .then(data => {
+            this.title = data.title
+            this.desc = data.desc
+            this.tech = data.tech
+            this.link = data.link
+            this.image = data.image
+            this.source = data.source
+        });
+    }
 })
 
 var app = new Vue({el: '#app', data: {}})
