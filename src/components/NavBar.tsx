0ffd91d4df071c6ea431b0b0ec7c6808141f0375
Jet Simon <jetjdsimon@gmail.com>
2025-02-17 17:20:24 -0800

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,18 @@
-import './NavBar.css'
 import { Link } from "react-router-dom";
+import './NavBar.css';
 
 function NavBar() {
     return (
         <div className='NavBar'>
-            <Link to="../">About Me</Link>
+            <Link to="/">About Me</Link>
             <span className="Spacer">|</span>
-            <Link to="../portfolio">Portfolio</Link>
+            <Link to="/portfolio">Portfolio</Link>
             <span className="Spacer">|</span>
-            <Link to="../blog">Blog</Link>
+            <Link to="/blog">Blog</Link>
             <span className="Spacer">|</span>
-            <a href="../JetSimonResume.pdf">Resume</a>
+            <a href="/JetSimonResume.pdf">Resume</a>
+            <span className="Spacer">|</span>
+            <a href="https://jetsimon.com/cool-sites/">Cool Sites</a>
         </div>
     )
 }
