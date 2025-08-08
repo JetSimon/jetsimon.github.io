166cc6ca695e9acedc8dcf73d3fbc4f52cec9330
Jet Simon <jetjdsimon@gmail.com>
2025-08-08 08:58:26 -0700

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,6 +13,8 @@ function NavBar() {
             <a href="/JetSimonResume.pdf">Resume</a>
             <span className="Spacer">|</span>
             <a href="https://jetsimon.com/cool-sites/">Cool Sites</a>
+            <span className="Spacer">|</span>
+            <a href="https://jetsimon.com/dailies/">Daily Games</a>
         </div>
     )
 }
