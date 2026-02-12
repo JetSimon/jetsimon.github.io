28a9cb8623c4d3f75ca4f3d56e233c876f615dbf
Jet Simon <jetjdsimon@gmail.com>
2026-02-12 07:12:08 -0800

    Add critter page

---
+++ src/AppleCritter.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import "./AppleCritter.css";
+
+interface CritterProps {
+    date: string;
+    title: string;
+    img: string;
+}
+
+function Critter(props: CritterProps) {
+    return (
+        <div className="Critter">
+            <h4>{props.title}</h4>
+            <img src={props.img}></img>
+            <p>{props.date}</p>
+        </div>
+    );
+}
+
+const critters: Array<CritterProps> = [
+    {
+        title: "Dr Critter",
+        img: "/critters/12-2-2026.jpg",
+        date: "2/12/2026"
+    }
+];
+
+function AppleCritter() {
+
+    const [critterIndex, setCritterIndex] = useState(critters.length - 1);
+
+    const critter = critters[critterIndex];
+
+    return (
+        <div className="AppleCritter">
+            <Critter title={critter.title} date={critter.date} img={critter.img}></Critter>
+            <div>
+                <div className="CritterButtons">
+                    <button disabled={critterIndex <= 0} onClick={() => { if (critterIndex > 0) setCritterIndex(critterIndex - 1); }}>Prev</button>
+                    <button disabled={critterIndex >= critters.length - 1} onClick={() => { if (critterIndex < critters.length - 1) setCritterIndex(critterIndex + 1); }}>Next</button>
+                </div>
+                <div className="CritterSelect">
+                    <label>Select Date</label>
+                    <select value={critterIndex} onChange={(e) => setCritterIndex(Number.parseInt(e.target.value))}>
+                        {critters.map((e, i) => <option key={i} value={i}>{e.date} - {e.title}</option>)}
+                    </select>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default AppleCritter;
--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import AboutMe from './AboutMe.tsx'
-import ErrorPage from './ErrorPage.tsx';
-import Portfolio from './Portfolio.tsx';
-import './index.css'
-import Layout from './Layout.tsx';
+import AboutMe from './AboutMe.tsx';
+import AppleCritter from './AppleCritter.tsx';
 import Blog from './Blog.tsx';
 import BlogPostList from './BlogPostList.tsx';
+import ErrorPage from './ErrorPage.tsx';
+import './index.css';
+import Layout from './Layout.tsx';
+import Portfolio from './Portfolio.tsx';
 
 const router = createBrowserRouter([
   {
-  element: <Layout></Layout>,
-  errorElement: <ErrorPage></ErrorPage>,
-  children: [
-    {
-      path: "/",
-      element: <AboutMe></AboutMe>,
-    },
-    {
-      path: "/portfolio",
-      element: <Portfolio></Portfolio>
-    },
-    {
-      path: "/blog",
-      element: <BlogPostList></BlogPostList>
-    },
-    {
-      path: "/blog/:postId",
-      element: <Blog></Blog>
-    }
-  ]
+    element: <Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <AboutMe></AboutMe>,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio></Portfolio>
+      },
+      {
+        path: "/blog",
+        element: <BlogPostList></BlogPostList>
+      },
+      {
+        path: "/blog/:postId",
+        element: <Blog></Blog>
+      },
+      {
+        path: "/critter",
+        element: <AppleCritter></AppleCritter>
+      }
+    ]
   }
 ]);
 
@@ -41,4 +46,4 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
